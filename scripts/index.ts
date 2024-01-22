f1bc2c8347b251f02a6caf33efd91d6eb32679d5
Jason Godesky <jefgodesky@gmail.com>
2024-01-22 13:14:29 -0500

    Reusable framework for loading client-side modules

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,3 +1,23 @@
+import selector from './selector'
 import setScrollbarWidth from './set-scrollbar-width'
 
 setScrollbarWidth()
+
+interface ModuleConfig {
+  elems: Element[] | null
+  name: string
+}
+
+const modules: ModuleConfig[] = [
+  { elems: selector('code.block'), name: 'code' }
+]
+
+const loadModules = async (configs: ModuleConfig[]): Promise<void> => {
+  for (const { elems, name } of configs) {
+    if (elems === null) continue
+    const module = await import(`./${name}/index`)
+    await module.default(elems)
+  }
+}
+
+void loadModules(modules)
