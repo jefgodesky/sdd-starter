b5497dce822163238f642004ddafa3de19ff771b
Jason Godesky <jefgodesky@gmail.com>
2024-04-05 20:30:42 -0400

    Color Theme Picker: setPreference: Call reflectPreference
    When we set your preference, reflect it immediately.

--- scripts/color-theme-picker/set-preference.ts
+++ scripts/color-theme-picker/set-preference.ts
@@ -1,5 +1,8 @@
+import reflectPreference from './reflect-preference'
+
 const setPreference = (pref: 'light' | 'dark'): void => {
   window.localStorage.setItem('color-theme-preference', pref)
+  reflectPreference(pref)
 }
 
 export default setPreference
